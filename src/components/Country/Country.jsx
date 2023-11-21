7cca2b33e929d3ac040f1793dda5dfd759cc0936
rifat1752 <rifathossain1752@gmail.com>
2023-11-21 23:52:38 +0600

    first commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,29 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-    const {name, flags} = country
-    console.log(country)
+const Country = ({country, handleVisitedCountry}) => {
+    const {name, flags,area,population} = country
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited=() =>{
+        setVisited(!visited);
+        
+
+    }
+
+    const passWithParams=()=>{
+        handleVisitedCountry(country);
+    }
     return (
-        <div className='box'>
+        <div className={`box ${visited && 'visited'}`}>
            <p>name: {name.common}</p> 
-           <img src={flags.png} alt="" />
+           <img className='flag' src={flags.png} alt="" />
+           <p>Area: {area}</p>
+           <p>Population:{population}</p>
+           <button onClick={passWithParams}  className='btn'>marked visited</button>
+           <button onClick={handleVisited} className='btn'>{visited ? 'visited':'going'}</button>
+           
         </div>
     );
 };
--- src/components/countries/Countris.jsx
+++ src/components/countries/Countris.jsx
@@ -1,23 +1,45 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './countries.css'
+
 
 const Countris = () => {
-
-    const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+
+
+    const handleVisitedCountry = country=>{
+        const newVisitedCountries = [...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries);
+        
+    }
+
     return (
-        <div>
-            <h3>countries: {countries.length}</h3>
-            {/* {
-                countries.map(country => <Country key={country.ccn3} country={country}></Country>)
-            } */}
+        <>
+         <h3>countries: {countries.length}</h3>
+         <h4>visited countries:{visitedCountries.length}</h4>
+         <div>
+         <ul>
+            {
+                visitedCountries.map(country => <li key={country.ccn3}>{country.name.common}</li>)
+            }
+
+         </ul>
+         </div>
+        <div className="countries">
+            {
+                countries.map(country => <Country key={country.ccn3} country={country} handleVisitedCountry={handleVisitedCountry}></Country>)
+            }
         </div>
+        </>
     );
 };
 
